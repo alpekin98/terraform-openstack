4e28801d46b7fbcfcf38fda222a2c4fdac5e3842
cem.topkaya <cem.topkaya@ulakhaberlesme.com.tr>
2024-04-29 12:45:01 +0000

    Added OpenStack CLI auto complete feature

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,8 @@
 
 pip install -r .devcontainer/requirements.txt 
 echo complete -C /usr/local/bin/terraform terraform >> ~/.bashrc 
+
+# -------------- Opensctack CLI Auto Complete ----------------
+# https://docs.openstack.org/python-openstackclient/pike/cli/command-objects/complete.html
+# Opensctack CLI aracının bash completion özelliğini aktif etmek için aşağıdaki komutu çalıştırın.
 openstack complete | tee /etc/bash_completion.d/osc.bash_completion > /dev/null
